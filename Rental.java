1c62802751ef88e5d479a7322e6a4ecfc9cb3564
Jang Seulgi <58168032+seulgi7707@users.noreply.github.com>
2020-12-11 23:37:53 +0900

    Add files via upload
    ex1

---
+++ Rental.java
@@ -0,0 +1,22 @@
+package com.ex1;
+
+//고객이 영화를 대여하는 것을 나타냄
+public class Rental {
+	private Movie _movie;
+	private int _daysRented;
+	
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+
+	public Movie getMovie() {
+		return _movie;
+	}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+	
+	
+}
